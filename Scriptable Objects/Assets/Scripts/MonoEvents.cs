cc4c73bae7bff12b8cc277580f4188e7143d79d6
Cameron <camhengland@gmail.com>
2024-09-02 22:11:30 -0600

    lab

---
+++ Scriptable Objects/Assets/Scripts/MonoEvents.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEvents : MonoBehaviour
+{
+    public UnityEvent awakeEvent, startEvent, onEnableEvent;
+    public float holdTime = 0.1f;
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(holdTime);
+        startEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        onEnableEvent.Invoke();
+    }
+}
